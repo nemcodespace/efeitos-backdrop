72ba78092600545873fe39d2ed47c9b1cdf9cae5
Nem code <nemcodespace@gmail.com>
2026-05-14 20:58:42 -0300

    atualizacao 1.0

---
+++ dados/dados.php
@@ -0,0 +1,51 @@
+<?php 
+
+$notificacoes = [
+  [
+    "img" => "../imgs/chrome.png",
+    "app" => "Chrome",
+    "text" => "Veja principais noticias CNN",
+  ],
+  
+  [
+    "img" => "../imgs/phone.png",
+    "app" => "Chamadas",
+    "text" => "Você tem 5 chamadas perdidas",
+  ],
+  
+  [
+    "img" => "../imgs/tim.png",
+    "app" => "Tim",
+    "text" => "Assine agora mesmo o tim club",
+  ],
+  
+  [
+    "img" => "../imgs/linkedin.png",
+    "app" => "linkedin",
+    "text" => "Analista de suporte enviou uma mensagem",
+  ],
+  
+  [
+    "img" => "../imgs/youtube-100.png",
+    "app" => "Youtube",
+    "text" => "Vamos usar óleo de baleia na madeira para dar vida",
+  ],
+  
+  [
+    "img" => "../imgs/google.png",
+    "app" => "Google",
+    "text" => "Nova atualização do aplicativo google docs",
+  ],
+  
+  [
+    "img" => "../imgs/app99.png",
+    "app" => "99 App",
+    "text" => "Nova corrida paga com sucesso",
+  ],
+  
+  [
+    "img" => "../imgs/clima.png",
+    "app" => "Clima",
+    "text" => "Veja o clima hoje Ventos fortes mais de 70km/h",
+  ],
+];
--- index.php
+++ index.php
@@ -1,12 +1,30 @@
+<?php include_once("./dados/dados.php")?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
   <title>Efeitos backdrop</title>
 </head>
 <body>
-  <h1>Ola mundo</h1>
+  <div class="container">
+    <?php 
+      foreach ($notificacoes as $items):?>
+      <div class="items">
+        <div class="img">
+          <img src="<?= $items["img"] ?>" alt="<?= $items["app"] ?>" width="100%">
+        </div>
+        <div class="text">
+          <h2><?= htmlspecialchars($items["app"]) ?></h2>
+          <p><?= htmlspecialchars(substr($items["text"], 0, 23) . "...") ?></p>
+        </div>
+        <div class="seta">
+          <img src="./icons/seta-expandir.png" alt="seta">
+        </div>
+      </div>
+    <?php endforeach ?>
+  </div>
 </body>
 </html>
